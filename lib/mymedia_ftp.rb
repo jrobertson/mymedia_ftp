1aa01a831b6df49ba237803a3e1e5a4be418c17a
Your Name <you@example.com>
2018-03-12 14:33:14 +0000

    feature: A file directory can now be copied or moved.

--- lib/mymedia_ftp.rb
+++ lib/mymedia_ftp.rb
@@ -8,17 +8,25 @@ require 'fileutils'
 
 class MyMediaFTP < Net::FTP
 
-  def initialize(host: '127.0.0.1', user: 'user', password: '1234', port: 21)
+  def initialize(host: '127.0.0.1', user: 'user', password: '1234', 
+                 port: 21, debug: false)
 
-    @curdir = Dir.pwd
+    @debug = debug
+    @curdir = '/'
     super()
     connect(host, port)
     login(user, password)
 
   end
+  
+  def chdir(dir)
+    super(dir)
+    @curdir = pwd
+  end
 
-  def cp(src='', dest='')
+  def cp(src='', dest='', &blk)
 
+    puts 'cp: ' + src.inspect if @debug
     chdir File.dirname(src)
     FileUtils.mkdir_p dest
     Dir.chdir  dest
@@ -27,38 +35,69 @@ class MyMediaFTP < Net::FTP
 
     puts 'copying ...'
 
-    files.each do |x|
+    files.each do |h|
 
-      puts x
-      getbinaryfile x, x.downcase.gsub(/ +/,'-')
-      yield(x) if block_given?
-    end
-
-  end
-
-
-  def mv(src='', dest='')
-
-    puts 'moving ...'
-    cp(src, dest) {|file| delete file }
-
-
-  end
-
-  private
-
-  def list_filenames(src)
-
-    raw_q = File.basename(src)
-
-    q = raw_q.gsub('.','\.').gsub('*','.*').gsub('?','.?')
-
-    list.inject([]) do |r, x| 
-
-      filename = x.split(/ +/,9).last
-      filename[/^#{q}$/] ? r << filename : r
+      name, type = h[:name], h[:type]
+      
+      puts name
+      
+      if type == :file then
+        getbinaryfile name, name.downcase.gsub(/ +/,'-')
+      else
+        cp_dir(name, &blk)
+      end
+      blk.call(name, type) if block_given?
     end
 
   end
   
+  def list_filenames(s=@curdir+'/*')
+    
+    if @debug
+      puts 'inside list_filenames' 
+      puts 's: ' + s.inspect
+    end
+    
+    src = File.dirname(s)
+
+    raw_q = File.basename(s)
+    puts 'raw_q: ' + raw_q.inspect if @debug
+    
+    q = raw_q.gsub('.','\.').gsub('*','.*').gsub('?','.?')\
+        .sub(/[^\*\?\.]$/,'.*')
+
+    list(src).inject([]) do |r, x| 
+
+      raw_attr, _, owner, group, filesize, month, day, time, filename = \
+          x.split(/ +/,9)
+      type = raw_attr =~ /d/ ? :directory : :file
+      filename[/^#{q}$/] ? r << {name: filename, type: type} : r
+      r
+    end
+
+  end
+  
+  alias ls list_filenames
+
+  def mv(src='', dest='')
+
+    puts 'moving ...'
+    cp(src, dest) do |file, type| 
+      type == :file ? delete(file) : rmdir(file)
+    end
+
+  end
+  
+  private
+  
+  def cp_dir(directory, &blk)
+    
+    puts 'inside cp_dir: ' + directory.inspect if @debug
+    FileUtils.mkdir_p directory
+    parent_dir = pwd
+    chdir directory
+    cp('*', directory, &blk)
+    chdir parent_dir
+  end
+    
 end
